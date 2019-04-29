3ec2868acd937d4fd9d5a5645e9dad6ad4a8258c
ksbasyigit <49840672+ksbasyigit@users.noreply.github.com>
2019-04-29 13:45:29 +0300

    Update Mosquitto-Kurulum.sh

--- Mosquitto-Kurulum.sh
+++ Mosquitto-Kurulum.sh
@@ -5,7 +5,7 @@ rm -rf mosquitto-repo.gpg.key
 cd /etc/apt/sources.list.d/
 wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
 apt update
-apt install mosquitto mosquitto-clients -y    
+apt install mosquitto mosquitto-clients -y
 cd /root/HAP-NodeJS
 npm install mqtt --save
 cd accessories/
